c9022b7f8c2432a8c1a1290e5430e0885760ce3b
xavier191 <43240528+xavier191@users.noreply.github.com>
2019-03-01 08:54:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$("button").click(function() {
+    var name=$(".name").val();
+    var books=$(".books").val();
+    var read=$(".days").val();
+    $("p").append("click");
+});
+
+
+    
+    
+  
+
